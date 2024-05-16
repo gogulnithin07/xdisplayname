770b0108e371df3dc31a864391600b85aa509397
martine9696 <gogulnithin07>
2024-05-16 21:23:28 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useState } from "react";
 function App() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setlastName] = useState("");
+  function handleSubmit(e) {
+    e.preventDefault();
+    const first = e.target.firstname.value;
+    const last = e.target.lastname.value;
+    setFirstName((c) => first);
+    setlastName((c) => last);
+    e.target.firstname.value = "";
+    e.target.lastname.value = "";
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <div>
+          <label>First name: </label>
+          <input name="firstname" type="text" required />
+        </div>
+        <div>
+          <label>last name: </label>
+          <input id="lastname" type="text" required />
+        </div>
+        <button type="submit">submit</button>
+      </form>
+      <p>
+        Full name:{firstName} {lastName}
+      </p>
     </div>
   );
 }
